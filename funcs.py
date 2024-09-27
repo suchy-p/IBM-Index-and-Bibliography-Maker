ada63bce90c7523786b92f325d42986d0981577f
Patryk <suchodolskipatryk@gmail.com>
2024-09-27 14:13:11 +0200

    Index and Bibliography maker ver 1.0

--- funcs.py
+++ funcs.py
@@ -1,26 +1,20 @@
 import re
 
-#from spacy.matcher import Matcher
+import PyPDF2
+import spacy
+from pygments.lexer import default
+from spacy.matcher import Matcher
+from spacy.pipeline.dep_parser import defaultdict
+from spacy.tokens import Span
 
-# delete, moved to main
-def add_matcher_pattern():
-    matcher = Matcher(secondary_nlp.vocab)
-
-    pattern = [
-        {"ENT_TYPE": "placeName"},
-        {"ENT_TYPE": "date"}
-    ]
-
-    matcher.add("adress_ending_pattern", [pattern])
 
 # func to add PERSON and processed page num to index
-def add_person(doc, secondary_nlp):
+def add_person(doc: spacy.tokens.Doc, secondary_nlp: spacy.Language) -> list:
     name_list = list()
     for ent in doc.ents:
 
         if ent.label_ == "PERSON":
 
-            # remove line breaking in text (Ro-\n bert)
             clean_index = remove_line_breaking(ent)
 
             # ent.lemma_ not working correctly for polish names; pl pipeline for handling declension issues
@@ -30,139 +24,97 @@ def add_person(doc, secondary_nlp):
             tokens = [token for token in doc_lemma if
                       token.text.isnumeric() == False]
 
-            # reverse list of tokens if contains more than just a surname
+            # reverse list of tokens if contains more than just a surname (Stanislaw Lem -> Lem Stanislaw)
             if len(tokens) > 1 and (tokens[1].text == "." or tokens[
                 -2].text == "."): tokens.insert(0, tokens.pop())
 
-            # make lemmas
+            # create lemmas
             surname = [token.lemma_ for token in tokens]
 
-            # remove leading and trailing spaces, lefotver punctuations
+            # remove leading and trailing spaces, leftover punctuation
             trimmed_surname = trimmer_index(surname)
 
-            # add trimmed surname as key, current page as value in "index" dict
-            # create list in this func, then return list from func and add
-            # all elements to index, current page as value still should be
-            # valid
             name_list.append(trimmed_surname.title())
-            #self.index[trimmed_surname.title()].add(
-            #    reader.get_page_number(current_page))
+
     return name_list
 
+def add_to_bibliography(
+        key: str, surname: str, current_page: PyPDF2.PageObject,
+        secondary_nlp: spacy.Language, matcher: spacy.matcher.Matcher) -> list:
 
-'''def add_person():
-    for ent in doc.ents:
-
-        if ent.label_ == "PERSON":
-
-            # remove line breaking in text (Ro-\n bert)
-            clean_index = remove_line_breaking(ent)
-
-            # ent.lemma_ not working correctly for polish names; pl pipeline for handling declension issues
-            doc_lemma = secondary_nlp(clean_index)
-
-            # remove nums sometimes left from footnotes or footnote references
-            tokens = [token for token in doc_lemma if
-                      token.text.isnumeric() == False]
-
-            # reverse list of tokens if contains more than just a surname
-            if len(tokens) > 1 and (tokens[1].text == "." or tokens[
-                -2].text == "."): tokens.insert(0, tokens.pop())
-
-            # make lemmas
-            surname = [token.lemma_ for token in tokens]
-
-            # remove leading and trailing spaces, lefotver punctuations
-            trimmed_surname = trimmer_index(surname)
-
-            # add trimmed surname as key, current page as value in "index" dict
-            index[trimmed_surname.title()].add(
-                reader.get_page_number(current_page))
-                '''
-
-def add_to_bibliography(key, name, current_page, secondary_nlp, matcher):
-    pattern = name
-    monography = rf"{pattern}[,]\s\D+[,].+\d+"
-    search = re.findall(monography, current_page.extract_text())
-    bibliography_from_page = list()
-    secondary_nlp = secondary_nlp
     matcher = matcher
+    pattern = surname
+    # regex for: surname, title etc.
+    publication_regex = rf"{pattern}[,]\s\D+[,].+\d+"
+    search = re.findall(publication_regex, current_page.extract_text())
+    secondary_nlp = secondary_nlp
 
-    for item in search:
-        if item[0].isupper():
-            trimmed = trimmer_bibliography(item, secondary_nlp, matcher)
-            no_line_breaks = remove_line_breaking(trimmed)
+    bibliography_from_current_page = list()
 
-            bibliography_from_page.append(no_line_breaks.replace(pattern, key))
+    for found_publication in search:
+        # probability check if found_publication starts with surname
+        if found_publication[0].isupper():
+            trimmed_publication = trimmer_bibliography(found_publication, secondary_nlp, matcher)
+            formatted_publication = remove_line_breaking(trimmed_publication)
+            # replace surname with key from index: 'Lem, Solaris...' -> 'Lem Stanislaw, Solaris...'
 
-    return bibliography_from_page
+            bibliography_from_current_page.append(formatted_publication.replace(pattern, key))
 
-# remove line breaking
-def remove_line_breaking(item):
+    return bibliography_from_current_page
+
+def remove_line_breaking(item: str | spacy.tokens.Span) -> str:
+    # remove line breaking in text (Ro-\n bert)
+    # used in index and bibliography
     no_line_breaks = item if type(item) is str else item.text
     remove = [" \xad ", " \xad\n", "\n", "\xa0"]
 
     for break_sign in remove:
-        no_line_breaks = no_line_breaks.replace(break_sign, " ") if (
-                break_sign == "\xa0") else no_line_breaks.replace(
-            break_sign, "")
+        no_line_breaks = no_line_breaks.replace(break_sign, " ") if (break_sign == "\xa0") \
+            else no_line_breaks.replace(break_sign, "")
 
     return no_line_breaks
 
-def trimmer_bibliography(i, secondary_nlp, matcher):
+def trimmer_bibliography(item: str, secondary_nlp: spacy.Language, matcher: spacy.matcher.Matcher) -> str:
 
-    # doc of given bibliohraphical address, both eng (doc1) and pl (doc2) pipelines
-    # doc1 = primary_nlp(i)
-    #doc2 = getattr(main.App, 'secondary_nlp')(i)
-    #secondary_nlp = main.app.secondary_nlp#getattr(main.app, 'secondary_nlp')
-    doc2 = secondary_nlp(i)
-    # index of place and year of publication
-    # eveything after this should be removed
-    # [author, title, year and place of publication]
-    place_and_year_index = int()
-    # reference_text =
-
-    #matches = getattr(main.App, 'matcher')(doc2)
-    #matcher = main.app.matcher#getattr(main.App, 'matcher')
-    matches = matcher(doc2)
+    # doc of given bibliographical address, pl pipeline
+    doc = secondary_nlp(item)
+    matches = matcher(doc)
 
     if len(matches) > 0:
         # get ending index of place and year of publication
         for match_id, start, end in matches:
-            matched_span = doc2[start:end]
+            matched_span = doc[start:end]
             place_and_year_index = matched_span.end
 
             # trimming bibliographical address, converting doc obj to str
-            trimmed = doc2[0: place_and_year_index].text + "."
+            trimmed = doc[0: place_and_year_index].text + "."
     else:
         # '\033[1m' + [...] + '\033[0m' spanning for bold text
-        trimmed = doc2[
-                  0::].text + '\033[1m' + "\t <<< \t check required" + '\033[0m'
+        trimmed = doc[0::].text + '\033[1m' + '\t <<< \t check required' + '\033[0m'
 
     return trimmed
 
-def trimmer_index(item):
-    # remove leftover spaces or punctuation; sometmies there are leftovers from footnotes or footnote references
-
+def trimmer_index(item: list[str]) -> str:
+    # remove leftover spaces or punctuation; sometimes there are leftovers from footnotes or footnote references
     # item is a list of lemmas, joining list items, removing leading and trailing spaces
     whitespace_trimmed = " ".join(item).strip()
-
     # removing "." and",", one more whitespace trimming in case there were any after punctuation
     punctuations_trimmed = whitespace_trimmed.lstrip("., ")
-
     # removing whitespace before "." after name initials [Smith J . => Smith J.; Smith A. J. => Smith A.J.]
     trimmed_item = punctuations_trimmed.replace(" .", ".").replace(". ", ".")
 
     return trimmed_item
 
-def write_bibliography_output(bibliography):
+
+def write_bibliography_output(bibliography: list[str]):
     output = sorted(set(bibliography))
     #with open ("bibliography_output.doc","w", encoding="utf-8" ) as file:
     with open("bibliography_output.txt", "w", encoding="utf-8") as file:
         for item in output:
             file.write(item+"\n")
 
-def write_index_output(index):
+
+def write_index_output(index: defaultdict[str|set]):
     with open("index_output.txt", "w", encoding="utf-8") as file:
     #with open ("index_output.doc","w", encoding="utf-8" ) as file:
         for key, subdict in sorted(index.items()):
@@ -172,6 +124,7 @@ def write_index_output(index):
             file.write(key+"\t")
             file.write(pages)
             file.write("\n")
+
 
 if __name__ == '__main__':
     app.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from collections import defaultdict
 import os
-import re
-from threading import Event, Thread
+from threading import Thread
 import tkinter as tk
 from tkinter import filedialog as fd
 import tkinter.ttk as ttk
@@ -13,42 +12,30 @@ from spacy.matcher import Matcher
 import funcs
 
 
-# reader object from given pdf
-#reader = PdfReader('C:\\Users\\YaTeż\\Downloads\\BP61-A3-Les.pdf')
-
-# primary nlp object,
-# "trf" = eng pipeline with highest accuracy for searching for entities
-               #load("en_core_web_trf"))
-
-# secondary nlp object,
-# for lemmatization of entities
-# eng pipeline can't handle declension in pl
-
-
-# index; defaultdict, ent.label == PERSON from doc as key,
-# set of pages as default value
-
-
-# if index_ready == True bibliography can be made
-
-
-# list for bibliography, later sorted for output
-
-
-# tkinter object
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry('400x200')
-        self.index_ready = False
-        self.primary_nlp = spacy.load('en_core_web_trf')
-        self.secondary_nlp = spacy.load('pl_core_news_lg')
         self.resizable(False, False)
         self.title('IBM')
-        self.page_num = 0
-        self.bibliography = list()
-        self.index = defaultdict(set)
+
+        # primary nlp object, "trf" = eng pipeline with the best accuracy for searching for entities
+        self.primary_nlp = spacy.load('en_core_web_trf')
+        # secondary nlp object for lemmatization of entities; eng pipeline can't handle declension in pl
+        self.secondary_nlp = spacy.load('pl_core_news_lg')
+        # matcher for bibliography, more in 'create_bibliography'
         self.matcher = Matcher(self.secondary_nlp.vocab)
+
+        # list for bibliography, later sorted for output
+        self.bibliography = list()
+        # index; defaultdict, ent.label == PERSON from doc as key, set of pages as default value
+        self.index = defaultdict(set)
+
+        # if index_ready == True bibliography can be made
+        self.index_ready = False
+        # num of analyzed page in reader object
+        self.page_num = 0
+
 
     def create_index(self, file_path):
 
@@ -59,10 +46,9 @@ class App(tk.Tk):
             current_page = reader.pages[self.page_num]
             doc = self.primary_nlp(current_page.extract_text())
             names_on_page = funcs.add_person(doc, self.secondary_nlp)
-            print(names_on_page)
+
             for name in names_on_page:
                 self.index[name].add(self.page_num+1)
-
 
             self.page_num += 1
 
@@ -72,76 +58,84 @@ class App(tk.Tk):
         self.enable_buttons()
         view.lbl3.config(text='Index file saved in pdf directory')
 
-    def create_bibliography(self, index, file_path, app):
+    def create_bibliography(self, file_path) :
 
         index = self.index
         reader = PdfReader(file_path)
 
+        # add search pattern for matcher; bibliographical address in polish system usually ends with place and year
+        # of publication -> [...], Krakow 2006
         pattern = [
             {"ENT_TYPE": "placeName"},
             {"ENT_TYPE": "date"}
         ]
 
         self.matcher.add("address_ending_pattern", [pattern])
-        #funcs.add_matcher_pattern()
-        print(index)
 
         for page in reader.pages:
+            # indexed names (keys) are used to search for publications on each page
             for key in sorted(index.keys()):
+
                 current_page = reader.pages[self.page_num]
-                print(current_page)
+                # split indexed name, use surname only for publications search
                 surname = key.split()[0]
-                print(surname)
-                self.bibliography.extend(funcs.add_to_bibliography(key, surname, current_page, self.secondary_nlp, self.matcher))
-                print(self.bibliography)
+                self.bibliography.extend(funcs.add_to_bibliography
+                                         (key, surname, current_page, self.secondary_nlp, self.matcher))
             self.page_num += 1
 
         funcs.write_bibliography_output(self.bibliography)
         view.lbl3.configure(text='Bibliography file saved in pdf directory')
+
 
     def disable_buttons(self):
         view.select_button.config(state='disabled')
         view.bibliography_button.config(state='disabled')
         view.index_button.config(state='disabled')
 
+
     def enable_buttons(self):
         view.select_button.config(state='enabled')
         view.bibliography_button.config(state='enabled')
         view.index_button.config(state='enabled')
+
 
     def get_file_path(self, label):
         file = fd.askopenfile(mode='r', filetypes=[('Pdf files','*.pdf')])
         file_path = os.path.abspath(file.name)
         os.chdir(os.path.dirname(file_path))
         view.lbl2.config(text=file_path, wraplength=355)
-        #index_ready = False
         view.index_button.configure(state='enabled')
+        # disable bibliography_button as precaution in case of changing input pdf after creating index
+        # or index and bibliography
         view.bibliography_button.configure(state='disabled')
         return file_path
+
 
     def thread1(self, func):
         t1 = Thread(target=self.index_run)
         t1.start()
 
-    def thread2(self, func):
-        t2 = Thread(target=self.bibliography_run)
-        t2.start()
 
     def index_run(self):
         view.lbl3.config(text='Creating personal index')
         self.disable_buttons()
         self.create_index(view.lbl2.cget('text'))
 
+
+    def thread2(self, func):
+        t2 = Thread(target=self.bibliography_run)
+        t2.start()
+
+
     def bibliography_run(self):
         view.lbl3.config(text='Creating bibliography from personal index')
         self.disable_buttons()
-        self.create_bibliography(self.index, view.lbl2.cget('text'), self.secondary_nlp)
+        self.create_bibliography(view.lbl2.cget('text'))#self.index, view.lbl2.cget('text'), self.secondary_nlp)
 
 
 class View:
     # tkinter view object
     def __init__(self):
-
         self.frame1 = tk.LabelFrame(width=355)
         self.lbl1 = tk.Label(text='Select pdf file')
         self.lbl2 = tk.Label(self.frame1, text='No file selected', anchor='center')
@@ -155,23 +149,31 @@ class View:
 app = App()
 view = View()
 
-
+# pdf file selection
 view.lbl1.grid(column=0, row=1, padx=20,pady=5)
 view.select_button.grid(column=1, columnspan = 1, row=1, padx=10, pady=10)
 view.select_button.bind('<Button-1>', app.get_file_path)
+# display path of selected file
 view.lbl2.pack()
+view.frame1.grid(column=0, columnspan=2, row=2, rowspan=2, padx=10, pady=15, sticky='ew')
+
 view.index_button.grid(column=0, row=4, pady=5)
 view.index_button.bind('<Button-1>', app.thread1)
+
 view.bibliography_button.grid(column=1, row=4, pady=5)
 view.bibliography_button.bind('<Button-1>', app.thread2)
+
+# output text messages
 view.lbl3.grid(column=0, columnspan=2, row=5, pady=10)
 
+# disable buttons before selecting pdf file
 view.bibliography_button.config(state='disabled')
 view.index_button.config(state='disabled')
 
-view.frame1.grid(column=0, columnspan=2, row=2, rowspan=2, padx=10, pady=15, sticky='ew')
+# divide window into two equal columns
 app.columnconfigure(0, weight=1)
 app.columnconfigure(1, weight=1)
+
 
 if __name__ == '__main__':
     app.mainloop()
